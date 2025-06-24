9764378621452ff7413a8ca8859c822b8be53e7d
Konnesain <200383445+Konnesain@users.noreply.github.com>
2025-06-24 21:19:25 +0700

    fixes

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir .rgrinstaller
-curl --output .rgrinstaller/rgr.tar.gz https://github.com/Konnesain/rgr/releases/latest/rgr.tar.gz
-tar -xzvf .rgrinstaller/rgr.tar.gz -C .rgrinstaller
+curl -Ls --output .rgrinstaller/rgr.tar.gz https://github.com/Konnesain/rgr/releases/latest/download/rgr.tar.gz
+tar -xzf .rgrinstaller/rgr.tar.gz -C .rgrinstaller
 sudo cp -f .rgrinstaller/rgraes.so .rgrinstaller/rgrviginere.so .rgrinstaller/rgrtransposition.so /usr/lib/
 sudo cp -f .rgrinstaller/rgr /usr/bin/
 rm -rf .rgrinstaller
---
+++ uninstaller.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo rm -f /usr/lib/rgraes.so /usr/lib/rgrtransposition.so /usr/lib/rgrviginere.so /usr/bin/rgr
